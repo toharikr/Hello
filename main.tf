44576837165473187966dbb71289987f314d6461
toharikr <53942441+toharikr@users.noreply.github.com>
2024-12-18 18:06:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,19 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Define variables
-variable "aws_region" {
-  description = "AWS region where the resources will be deployed"
-}
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-}
-
-variable "instance_type" {
-  description = "Type of EC2 instance"
-}
-
 # Create an EC2 instance
 resource "aws_instance" "example" {
   ami           = var.ami_id
