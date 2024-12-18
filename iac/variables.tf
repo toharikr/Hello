cfb5f9b1dc0485ddf6df805d16d8f91b448c9ebb
toharikr <53942441+toharikr@users.noreply.github.com>
2024-12-18 17:54:53 -0500

    Create variables.tf

---
+++ iac/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the instance"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+}
